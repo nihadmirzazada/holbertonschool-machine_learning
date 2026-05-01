bae464feaa256a6cd001de8cb440fd308accc1fa
nihadmirzazada <13638@holbertonstudents.com>
2026-05-01 23:42:23 +0000

    fix 5

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -7,7 +7,8 @@ def definiteness(matrix):
     """Returns the definiteness of a matrix as a string, or None"""
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1] or matrix.size == 0:
+    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]\
+            or matrix.size == 0:
         return None
     if not np.allclose(matrix, matrix.T):
         return None
